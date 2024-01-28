d97fad8b0a1c79acba4c07261cc50f3d0f313910
seokgyeong <amisgyeong@gmail.com>
2024-01-28 20:15:35 +0900

    게시판 CRUD 게시글 수정

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,16 @@
 import PostForm from "components/posts/PostForm";
 import PostBox from "components/posts/PostBox";
+import { useContext, useEffect, useState } from "react";
+
+import {
+  collection,
+  query,
+  where,
+  onSnapshot,
+  orderBy,
+} from "firebase/firestore";
+import AuthContext from "context/AuthContext";
+import { db } from "firebaseApp";
 
 export interface PostProps {
   id: string;
@@ -13,65 +24,44 @@ export interface PostProps {
   comments?: any;
 }
 
-const posts: PostProps[] = [
-  {
-    id: "1",
-    email: "test@test.com",
-    content: "내용입니다.",
-    createAt: "2023-08-30",
-    uid: "123123",
-  },
-  {
-    id: "2",
-    email: "test@test.com",
-    content: "내용입니다.",
-    createAt: "2023-08-30",
-    uid: "123123",
-  },
-  {
-    id: "3",
-    email: "test@test.com",
-    content: "내용입니다.",
-    createAt: "2023-08-30",
-    uid: "123123",
-  },
-  {
-    id: "4",
-    email: "test@test.com",
-    content: "내용입니다.",
-    createAt: "2023-08-30",
-    uid: "123123",
-  },
-  {
-    id: "5",
-    email: "test@test.com",
-    content: "내용입니다.",
-    createAt: "2023-08-30",
-    uid: "123123",
-  },
-  {
-    id: "6",
-    email: "test@test.com",
-    content: "내용입니다.",
-    createAt: "2023-08-30",
-    uid: "123123",
-  },
-];
-
 export default function HomePage() {
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (user) {
+      let postsRef = collection(db, "posts");
+      let postsQuery = query(postsRef, orderBy("createAt", "desc"));
+
+      onSnapshot(postsQuery, (snapShot) => {
+        let dataObj = snapShot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc?.id,
+        }));
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [user]);
+
   return (
     <div className="home">
-      <div className="home__title">Home</div>
-      <div className="home__tabs">
-        <div className="home__tab home__tab--active">For You</div>
-        <div className="home__tab">Following</div>
+      <div className="home__top">
+        <div className="home__title">Home</div>
+        <div className="home__tabs">
+          <div className="home__tab home__tab--active">For You</div>
+          <div className="home__tab">Following</div>
+        </div>
       </div>
       <PostForm />
       {/* Tweet Posts */}
       <div className="post">
-        {posts?.map((post) => (
-          <PostBox post={post} key={post.id} />
-        ))}
+        {posts?.length > 0 ? (
+          posts?.map((post) => <PostBox post={post} key={post.id} />)
+        ) : (
+          <div className="post__no-posts">
+            <div className="posts__text">뭐라고 말좀해</div>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/posts/edit.tsx
+++ src/pages/posts/edit.tsx
@@ -1,3 +1,5 @@
+import PostEditForm from "components/posts/PostEditForm";
+
 export default function PostEdit() {
-  return <div>Post Edit Page</div>;
+  return <PostEditForm />;
 }
