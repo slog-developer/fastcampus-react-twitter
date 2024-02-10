0c9847953d27bf38d8f59297e2df68c2ea13db04
seokgyeong <amisgyeong@gmail.com>
2024-02-11 08:10:14 +0900

    해시태그 검색 구현

--- src/components/posts/PostForm.tsx
+++ src/components/posts/PostForm.tsx
@@ -7,7 +7,9 @@ import { toast } from "react-toastify";
 import AuthContext from "context/AuthContext";
 
 export default function PostForm() {
-  const [content, setContent] = useState<string>();
+  const [content, setContent] = useState<string>("");
+  const [hashTag, setHashTag] = useState<string>("");
+  const [tags, setTags] = useState<string[]>([]);
   const { user } = useContext(AuthContext);
   const handleFileUpload = () => {};
 
@@ -24,7 +26,10 @@ export default function PostForm() {
         }),
         uid: user?.uid,
         email: user?.email,
+        hashTags: tags,
       });
+      setTags([]);
+      setHashTag("");
       setContent("");
       toast.success("게시글을 생성했습니다.");
     } catch (e: any) {
@@ -42,6 +47,27 @@ export default function PostForm() {
     }
   };
 
+  const onChangeHashTag = (e: any) => {
+    setHashTag(e?.target.value?.trim());
+  };
+
+  const handleKeyUp = (e: any) => {
+    if (e.keyCode === 32 && e.target.value.trim() !== "") {
+      // 만약 같은 태그가 있다면 에러를 띄운다.
+      // 태그를 생성해준다.
+      if (tags?.includes(e.target.value?.trim())) {
+        toast.error("같은 태그가 있습니다.");
+      } else {
+        setTags((prev) => (prev?.length > 0 ? [...prev, hashTag] : [hashTag]));
+        setHashTag("");
+      }
+    }
+  };
+
+  const removeTag = (tag: string) => {
+    setTags(tags?.filter((val) => val !== tag));
+  };
+
   return (
     <form className="post-form" onSubmit={onSubmit}>
       <textarea
@@ -53,6 +79,29 @@ export default function PostForm() {
         onChange={onChange}
         value={content}
       />
+      <div className="post-form__hashtags">
+        <span className="post-form__hashtags-outputs">
+          {tags?.map((tag, index) => (
+            <span
+              className="post-form__hashtags-tag"
+              key={index}
+              onClick={() => removeTag(tag)}
+            >
+              #{tag}
+            </span>
+          ))}
+        </span>
+        <input
+          type="text"
+          className="post-form__input"
+          name="hashtag"
+          id="hashtag"
+          placeholder="해시태그 + 스페이스바 입력"
+          onChange={onChangeHashTag}
+          onKeyUp={handleKeyUp}
+          value={hashTag}
+        />
+      </div>
       <div className="post-form__submit-area">
         <label htmlFor="file-input" className="post-form__file">
           <FiImage className="post-form__file-icon" />
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,3 +1,69 @@
-export default function Search() {
-  return <>Search Page</>;
+import PostBox from "components/posts/PostBox";
+import AuthContext from "context/AuthContext";
+import {
+  collection,
+  where,
+  query,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
+import { db } from "firebaseApp";
+import { PostProps } from "pages/home";
+import { useContext, useEffect, useState } from "react";
+
+export default function SearchPage() {
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const [tagQuery, setTagQuery] = useState<string>("");
+  const { user } = useContext(AuthContext);
+
+  const onChange = (e: any) => {
+    setTagQuery(e?.target?.value?.trim());
+  };
+
+  useEffect(() => {
+    if (user) {
+      let postRef = collection(db, "posts");
+      let postQuery = query(
+        postRef,
+        where("hashTags", "array-contains-any", [tagQuery]),
+        orderBy("createdAt", "desc")
+      );
+
+      onSnapshot(postQuery, (snapShot) => {
+        let dataObj = snapShot?.docs?.map((doc) => ({
+          ...doc?.data(),
+          id: doc?.id,
+        }));
+
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [tagQuery, user]);
+
+  return (
+    <div className="home">
+      <div className="home__top">
+        <div className="home__title">
+          <div className="home__title-text">Search</div>
+        </div>
+        <div className="home__search-div">
+          <input
+            type="text"
+            className="home__search"
+            placeholder="해시태그 검색"
+            onChange={onChange}
+          />
+        </div>
+      </div>
+      <div className="post">
+        {posts?.length > 0 ? (
+          posts?.map((post: PostProps) => <PostBox post={post} key={post.id} />)
+        ) : (
+          <div className="post__no-posts">
+            <div className="posts__text">게시글이 없습니다.</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
